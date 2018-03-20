f27bf9fe7282a84879bdb74a1a30978c562ce849
George Samuels II <mrsonicthunder@gmail.com>
2018-03-20 03:47:52 -0400

    Test run UI
    Testing UI in real time; counters etc; baby stuff

--- Assets/scripts/Food.cs
+++ Assets/scripts/Food.cs
@@ -2,21 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Food : MonoBehaviour {
 
 	public GameObject fruitSlicedPrefab;
 	public float startForce = 15f;
 
+	public static int g=0,f=0,v=0,d=0,p=0,o=0;
+	public static Food instance;
+
+	//int[] foods;
 	Rigidbody2D rig;
+
 
 	void Start ()
 	{
 		rig = GetComponent<Rigidbody2D>();
 		rig.AddForce (transform.up * startForce, ForceMode2D.Impulse);
+		//one.text = g.ToString ();
+//		foods =new int[foods.Length];
+
+		//FillArray ();// place zero in every spot
+
+
+		instance = this;
+
+	}
+	void Update()
+	{
+		//one.text = g.ToString ();
 	}
 
 	void OnTriggerEnter2D (Collider2D col)
 	{
+		
+
+
 		if (col.tag == "swipe")
 		{
 			Debug.Log ("food hit");
@@ -27,7 +48,86 @@ public class Food : MonoBehaviour {
 			GameObject slicedFruit = Instantiate(fruitSlicedPrefab, transform.position, rotation);
 			Destroy(slicedFruit, 1f);
 			Destroy(gameObject);
+
+
+			switch (fruitSlicedPrefab.tag) 
+			{
+			case "grain":
+				//add to grain
+				g++;
+				//Debug.Log("counting" + "==>" + g);
+			//strt=	foods[0]++;
+			
+				break;
+			case "veggie":
+				//add to veggie
+				v++;
+				//strt=	foods[1]++;
+				break;
+			case "protein":
+				//add to protein
+				p++;
+				//strt=foods[2]++;
+				break;
+
+
+
+			case "dairy":
+				//add to dairy
+				d++;
+				//strt=foods[3]++;
+				break;
+			case "fruit":
+				//add to fruits
+				f++;
+			//	strt=foods[4]++;
+				break;
+			case "oils":
+				//add to oils
+				o++;
+			//	strt=foods[5]++;
+			break;
+
+
+
+			}
+
+
+
 		}
 	}
+
+	/**void FillArray()
+	{
+		for (int i = 0; i < foods.Length; i++)
+		{
+			foods [i] = strt;
+		}
+	}
+**/
+
+	public int Counting()
+	{
+		
+
+
+		//add to grain
+			return g;
+
+
+
+
+		//	return strt ;
+
+
+	}
+
+
+
+
+
+
+
+
 
 }
---
+++ Assets/scripts/UI/Counter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Counter : MonoBehaviour {
+	public Text UI;
+
+	public Text score;
+	//public Text countg;
+	// Use this for initialization
+	void Start () {
+
+		//UI= new Text[UI.Length];
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		//for(int i=0;i<UI.Length;i++)
+		UI.text = Food.instance.Counting ().ToString ();
+
+		if(!UI.text.Equals("0"))
+			score.text ="Score: " + (Food.instance.Counting ()*100).ToString ();
+		//UI[2].text = Food.instance.Counting ().ToString ();
+	}
+}
